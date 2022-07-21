e7aebaf4e2eac7a40afb2f63395e5c5ecda1bc65
winston kiprop <winstonekip50@gmail.com>
2022-07-21 11:47:38 +0300

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,17 @@ function NavBar() {
   const links = ["home", "about", "projects"];
 
   return <nav>{/* display an <a> tag for each link here */}</nav>;
+   <a href="#home"></a>
+  const anchors = links.map((link) => (
+    <a key={link} href={ "#" + link}>
+      {link}
+    </a>
+  ))
+  return <nav>
+    {anchors}
+  </nav>;
+
 }
 
 export default NavBar;
+
